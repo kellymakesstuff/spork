85b2871e588f97bff771d4a896a7c2a60c7ab7d8
Felicia Grace Csolak <feliciacsolak@Felicias-MacBook-Pro.local>
2020-06-19 16:16:47 -0400

    Main.css updated and added images folder

--- client/spork-app/src/App.js
+++ client/spork-app/src/App.js
@@ -4,24 +4,22 @@ import React, { useEffect, useState } from 'react'
 
 
 export default function App() {
-  
+
   const [recipe, updateRecipe] = useState([])
-  
-  
+
   useEffect( async() => {
     let data = await getRecipes()
     console.log(data)
     updateRecipe(data)
     // console.log(recipe)
   },[])
-  
+
 
 
   return (
     <div>
       <h2>Hello</h2>
-      
-  
+
     </div>
   )
 }

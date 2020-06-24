a6dbae0c102ff7e0e4760d3ce2901e12074f9e0c
Felicia Grace Csolak <feliciacsolak@Felicias-MacBook-Pro.local>
2020-06-24 18:27:47 -0400

    beginning of CSS overhaul

--- client/spork-app/src/components/Home.jsx
+++ client/spork-app/src/components/Home.jsx
@@ -84,45 +84,6 @@ class Home extends Component {
     // }
   }
 
-
-
-  carouselDataFilter = async () => {
-    const response = await getRecipes()
-    let starData = []
-    let veggieData = []
-    let meatData = []
-    for (let i = 0; i < response.length; i++) {
-      if (response[i].starRating === 5) {
-        starData.push(response[i])
-      } else if (response[i].dishName.includes("Vegetarian") === true) {
-        veggieData.push(response[i])
-      } else if (response[i].dishName.includes("Chicken") === true || response[i].dishName.includes("Beef") === true || response[i].dishName.includes("Pork") === true) {
-        meatData.push(response[i])
-      }
-    }
-    this.setState({
-      fiveStarRecipes: starData,
-      vegetarianRecipes: veggieData,
-      meatRecipes: meatData
-    })
-  }
-
-  randomizeData = async () => {
-    const response = await getRecipes()
-    let randomized = []
-    // let randomNum = Math.floor(Math.random() * response.length)
-    while (randomized.length < 10) {
-      let randomNum = Math.floor(Math.random() * response.length)
-      let rRecipe = response[randomNum]
-      randomized.push(rRecipe)
-    }
-
-    this.setState({
-      randomizedRecipes: randomized
-    })
-
-  }
-
   render() {
     return (
       <div>

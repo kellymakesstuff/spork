e2d2d535d558bef1670c34fa9da53979c27596cc
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-06-25 16:45:36 -0400

    about done

--- client/spork-app/src/components/About.jsx
+++ client/spork-app/src/components/About.jsx
@@ -11,10 +11,24 @@ export default function About() {
       <div className="header">
         About the Team
         </div>
+
       <div className="about-container">
         <h3>Spork was developed by students at General Assembly NYC. This on-going project began June 2020.</h3>
+
+        <div className="ux-about-header">
+          <h2>UX/UI Team</h2>
+          <div className="ux-team">
+            <div className="ashley"><a href="https://www.linkedin.com/in/ashley-gluck-56134b1b1/"><img src={LinkedIn} /></a><p>Ashley Gluck</p></div>
+            <div className="Juliza"><a href="https://github.com/rizekj12"><img src={LinkedIn} /></a><p>Juliza Vasquez</p></div>
+            <div className="stephanie">
+              <a href="https://www.linkedin.com/in/stephanie-cardenas-981b2712b/"><img src={LinkedIn} /></a><p>Stephanie Cardenas</p></div>
+
+          </div>
+        </div>
         <div className="sei-about-header">
           <h2>SEI Team</h2>
+
+
           <div className="sei-team">
             <div className="felicia">
               <a href="https://github.com/felicia-csolak"><img src={GitHub} /></a><p>Felicia Csolak</p></div>
@@ -25,15 +39,9 @@ export default function About() {
             </div>
           </div>
         </div>
-        <div className="ux-about-header">
-          <h2>UX/UI Team</h2>
-          <div className="ux-team">
-            <a href="https://www.linkedin.com/in/ashley-gluck-56134b1b1/"><img src={LinkedIn} /></a><p>Ashley Gluck</p>
-            <a href="https://github.com/rizekj12"><img src={LinkedIn} /></a><p>Juliza Vasquez</p>
-            <a href="https://www.linkedin.com/in/stephanie-cardenas-981b2712b/"><img src={LinkedIn} /></a><p>Stephanie Cardenas</p>
 
-          </div>
-        </div>
+
+        <h3>additional substitute API information from <span><a href="www.spoonacular.com">Spoonacular</a></span></h3>
       </div>
     </>
   )

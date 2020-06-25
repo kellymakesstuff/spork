8f34f3e4c8a5857c0de747d12c0f545228c0a9ee
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-06-25 18:50:42 -0400

    review refactored, added allReview state

--- client/spork-app/src/components/Reviews.jsx
+++ client/spork-app/src/components/Reviews.jsx
@@ -5,95 +5,102 @@ import { withRouter, Link } from "react-router-dom"
 import CondensedHeader from './shared/CondensedHeader'
 
 class Reviews extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            review: {
-                name: "",
-                comment: "",
-                starRating: 0
-            }
+  constructor(props) {
+    super(props)
+    this.state = {
+      review: {
+        name: "",
+        comment: "",
+        starRating: 0
+      },
+
+      allReviews: null
+    }
+  }
+
+
+  componentDidMount = async () => {
+    const filteredRecipe = await this.props.commentData.find((recipe) => recipe._id === this.props.match.params.id)
+    this.setState({ allReviews: filteredRecipe.comments })
+  }
+
+  handleNameChange = (e) => {
+    this.setState({
+      review: {
+        ...this.state.review,
+        name: e.target.value
+      }
+    })
+  }
+
+  handleCommentChange = (e) => {
+    this.setState({
+      review: {
+        ...this.state.review,
+        comment: e.target.value
+      }
+    })
+  }
+
+  handleRatingChange = (e) => {
+    this.setState({
+      review: {
+        ...this.state.review,
+        starRating: e.target.value
+      }
+    })
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault()
+    let { id } = this.props.match.params
+    const created = await createComment(id, this.state.review)
+    this.setState(prevState => ({
+      allReviews: [...prevState.allReviews, created]
+    }))
+
+    console.log(this.state.allReviews)
+  }
+
+
+  render() {
+    const filteredRecipe = this.props.commentData.find((recipe) => recipe._id === this.props.match.params.id)
+    // console.log(filteredRecipe)
+    return (
+      <div>
+        {filteredRecipe &&
+          <Link to={`/search/${this.props.inputValue}/${filteredRecipe._id}`}><CondensedHeader /></Link>
         }
-    }
+        <div className="header">Reviews</div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Name" onChange={this.handleNameChange} />
+          <input type="text" placeholder="Comment" onChange={this.handleCommentChange} />
+          <input type="number" placeholder="Star rating" onChange={this.handleRatingChange} />
 
-    
 
-    handleNameChange = (e) => {
-        this.setState({
-            review: {
-                ...this.state.review,
-                name: e.target.value
-            }
-        })
-    }
+          <button>Submit</button>
+        </form>
+        <h1>Reviews for:</h1>
+        {filteredRecipe &&
+          <div>{filteredRecipe.dishName}</div>}
 
-    handleCommentChange = (e) => {
-        this.setState({
-            review: {
-                ...this.state.review,
-                comment: e.target.value
-            }
-        })
-    }
 
-    handleRatingChange = (e) => {
-        this.setState({
-            review: {
-                ...this.state.review,
-                starRating: e.target.value
-            }
-        })
-    }
 
-    handleSubmit = async (e) => {
-        // e.preventDefault()
-        let { id } = this.props.match.params
-        const created = await createComment(id, this.state.review)
-        this.setState({
-            created
-        })
 
-    }
-    
-
-    render() {
-      const filteredRecipe = this.props.commentData.find((recipe) => recipe._id === this.props.match.params.id)
-      // console.log(filteredRecipe)
-        return (
+        {this.state.allReviews && this.state.allReviews.map(review =>
           <div>
-            {filteredRecipe &&
-              <Link to={`/search/${this.props.inputValue}/${filteredRecipe._id}`}><CondensedHeader /></Link>
-            }
-            <div className="header">Reviews</div>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" placeholder="Name" onChange={this.handleNameChange} />
-                    <input type="text" placeholder="Comment" onChange={this.handleCommentChange} />
-                    <input type="number" placeholder="Star rating" onChange={this.handleRatingChange} />
+            <h3>{review.name}</h3>
+            <BeautyStars value={review.starRating} size="10px" />
+            <h3>{review.comment}</h3>
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
 
-                    
-                    <button>Submit</button>
-            </form>
-            <h1>Reviews for:</h1>
-            {filteredRecipe &&
-              <div>{filteredRecipe.dishName}</div>}
-            
-            
+        )}
 
-
-            {filteredRecipe && filteredRecipe.comments.map(review => 
-              <div>
-                <h3>{review.name}</h3>
-                <BeautyStars value={review.starRating} size="10px" />
-                <h3>{review.comment}</h3>
-                <button>Edit</button>
-                <button>Delete</button>
-              </div>
-              
-              )}
-    
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 export default withRouter(Reviews) 
--- client/spork-app/src/services/apiSetup.js
+++ client/spork-app/src/services/apiSetup.js
@@ -4,13 +4,14 @@ let apiUrl
 
 const apiUrls = {
   production: "https://spork-project3.herokuapp.com/api",
+  // development: "https://spork-project3.herokuapp.com/api",
   development: "http://localhost:3001/api"
 }
 
 if (window.location.hostname === "localhost") {
   apiUrl = apiUrls.development
 } else {
-  apiUrl = apiUrls.production 
+  apiUrl = apiUrls.production
 }
 const api = axios.create({
   baseURL: apiUrl,

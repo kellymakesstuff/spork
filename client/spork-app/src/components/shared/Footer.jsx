2ae7bae3a2e5e791e43422f49d1ea9f0da189aba
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-06-26 13:05:58 -0400

    fixrf link

--- client/spork-app/src/components/shared/Footer.jsx
+++ client/spork-app/src/components/shared/Footer.jsx
@@ -4,7 +4,7 @@ import { withRouter, Link, Route } from "react-router-dom"
 export default function Footer() {
   return (
     <div className="footer">
-      <Link to="/about"><h4>About the Team</h4></Link>
+      <Link to="/about"><h4 className="its-us">About the Team</h4></Link>
     </div>
   )
 }

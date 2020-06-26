377056f04709747ae75e1e3a3671957159759236
Felicia Grace Csolak <feliciacsolak@Felicias-MacBook-Pro.local>
2020-06-25 20:32:04 -0400

    recipe results CSS fixed

--- client/spork-app/src/components/RecipeResults.jsx
+++ client/spork-app/src/components/RecipeResults.jsx
@@ -27,6 +27,7 @@ function RecipeDetail(props) {
     <div>
       <Link to="/"> <CondensedHeader /> </Link>
       <div className="header">Recipe Results</div>
+      <div className="recipe-results-master-container">
       <div className="recipe-results-container">
       {filteredData.map(recipes =>
         <div className="filter-detail" key={recipes.dishName}>
@@ -40,6 +41,7 @@ function RecipeDetail(props) {
           <p>{recipes.prepTime}</p>
         </div>
       )}
+</div>
 </div>
     </div>
   )

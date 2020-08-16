6c4120f6dbcf7cd632417e9002780f94488b917c
zainsattar18 <zainsattar18@gmail.com>
2020-08-16 13:02:33 -0400

    fixed the updated added modal for better visual and added styling to review pages and modal pages

--- client/spork-app/src/components/Reviews.jsx
+++ client/spork-app/src/components/Reviews.jsx
@@ -1,9 +1,10 @@
-  import React, { Component } from 'react'
+import React, { Component } from 'react'
 import { createComment, deleteComments, updateComments } from "../services/recipes"
 import BeautyStars from "beauty-stars"
 import { withRouter, Link } from "react-router-dom"
 import CondensedHeader from './shared/CondensedHeader'
-import UpdateReview from "./UpdateReview"
+// import UpdateReview from "./UpdateReview"
+import Modal from 'react-modal'
 import "../css/Reviews.css"
 
 class Reviews extends Component {
@@ -13,7 +14,9 @@ class Reviews extends Component {
       review: {
         name: "",
         comment: "",
-        starRating: 0
+        starRating: 0,
+        isOpen: false,
+        update: ''
       },
 
       allReviews: null,
@@ -34,6 +37,31 @@ class Reviews extends Component {
       }
     })
   }
+
+  openModal = () => {
+    this.setState({
+      isOpen: true
+    })
+  }
+
+  hideModal = () => {
+    this.setState({
+      isOpen: false
+    })
+  }
+
+  updateModal = (id) => {
+    this.setState({
+      update: id
+    })
+  }
+
+  hideUpdateModal = () => {
+    this.setState({
+      update: false
+    })
+  }
+
 
   handleCommentChange = (e) => {
     this.setState({
@@ -75,8 +103,8 @@ class Reviews extends Component {
     })
   }
 
-  handleUp= async (id, comments) => {
-    
+  handleUpdate = async (id, comments) => {
+
     let update = await updateComments(id, this.state.review)
     const allReviews = this.state.allReviews.map(review => (
       review._id === id ? update : review
@@ -104,20 +132,22 @@ class Reviews extends Component {
         <div className="reviewAndForm">
           <h2>Submit a Review</h2>
           <form className="reviewForm" onSubmit={this.handleSubmit}>
-            <input className="reviewInput" type="text" placeholder="Name" onChange={this.handleNameChange} value={this.state.review.name}/>
-            <input className="reviewInput commentInput" type="text" placeholder="Comment" onChange={this.handleCommentChange} value={this.state.review.comment}/>
-            <input className="reviewInput" type="number" placeholder="Star rating" onChange={this.handleRatingChange} value={this.state.review.starRating}/>
+            <input className="reviewInput" type="text" placeholder="Name" onChange={this.handleNameChange} value={this.state.review.name} />
+            <textarea className="commentInput" type="text" placeholder="Comment" onChange={this.handleCommentChange} value={this.state.review.comment} />
+            <input className="star-rating" type="number" placeholder="Star rating" onChange={this.handleRatingChange} value={this.state.review.starRating} min="0"
+              max="5" />
 
-
-            <button className="reviewButton" >Submit</button>
+            <div className="submit">
+              <button className="submit-button">Submit</button>
+            </div>
           </form>
 
 
-          <div>
+          {/* <div>
             {this.state.isEmptyState && <editButton addTrip={this.triggerAddTripState} />}
 
             {this.state.isAddTripState && <UpdateReview props={this.state} />}
-          </div>
+          </div> */}
 
           <h1>Reviews for:</h1>
           {filteredRecipe &&
@@ -131,8 +161,43 @@ class Reviews extends Component {
               <h3>{review.name}</h3>
               <BeautyStars value={review.starRating} size="10px" />
               <h3>{review.comment}</h3>
-              <button onClick={() => this.handleUp(review._id)}>Edit Review</button>
-              <button onClick={() => this.handleDelete(review._id)}>Delete</button>
+              <button className="update-button" onClick={this.openModal}>Update Review</button>
+              <Modal
+                isOpen={this.state.isOpen}
+                onRequestClose={this.hideModal}
+                contentLabel="Example Modal"
+              >
+                 <button className="hidemodal" onClick={this.hideModal}>X</button>
+                <h2 className="reviewAndForm">Edit Review</h2>
+                <div className="center-form">
+                  <form className="edit-form" onSubmit={this.handleSubmit}>
+                    <input
+                      className="edit-name"
+                      type="text" placeholder="Name"
+                      onChange={this.handleNameChange}
+                      value={this.state.review.name} />
+                    <input
+                      className="edit-comment"
+                      type="text" placeholder="Comment"
+                      onChange={this.handleCommentChange}
+                      value={this.state.review.comment} />
+                    <input
+                      className="edit-rating"
+                      type="number"
+                      placeholder="Star rating"
+                      onChange={this.handleRatingChange}
+                      value={this.state.review.starRating}
+                      min="0"
+                      max="5"
+                    />
+
+                  </form>
+                </div>
+                <div className="submit">
+                  <button className="edit-button" onClick={() => this.handleUpdate(review._id)}>Edit Review</button>
+                </div>
+              </Modal>
+              <button className="delete-button" onClick={() => this.handleDelete(review._id)}>Delete</button>
               <hr />
             </div>
 
--- client/spork-app/src/components/UpdateReview.jsx
+++ client/spork-app/src/components/UpdateReview.jsx
@@ -1,76 +1,114 @@
-// import React, { Component } from 'react'
-// import { createComment } from "../services/recipes"
-// import BeautyStars from "beauty-stars"
-// import { withRouter, Link } from "react-router-dom"
-// import CondensedHeader from './shared/CondensedHeader'
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom';
+import '../css/UpdateReview.css'
 
 
-// class UpdateReview extends Component {
-//   constructor(props) {
-//     super(props)
+class UpdateReview extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      comment: '',
+      starRating: ''
+    }
+  }
 
-//   }
+  componentDidMount() {
+    if (this.props.reviewItem) {
+      this.setFormData();
+    }
+  }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.reviewItem !== this.props.reviewItem) {
+      this.setFormData();
+    }
+  }
 
-//   // componentDidMount = async () => {
-//   //   const filteredRecipe = await this.props.commentData.find((recipe) => recipe._id === this.props.match.params.id)
-//   //   this.setState({ allReviews: filteredRecipe.comments })
-//   // }
+  setFormData = () => {
+    this.setState({
+      name: this.props.reviewItem.name,
+      comment: this.props.reviewItem.comment,
+      starRating: this.props.reviewItem.starRating
+    })
+  }
 
-//   handleNameChange = (e) => {
-//     this.setState({
-//       review: {
-//         ...this.state.review,
-//         name: e.target.value
-//       }
-//     })
-//   }
-
-//   handleCommentChange = (e) => {
-//     this.setState({
-//       review: {
-//         ...this.state.review,
-//         comment: e.target.value
-//       }
-//     })
-//   }
-
-//   handleRatingChange = (e) => {
-//     this.setState({
-//       review: {
-//         ...this.state.review,
-//         starRating: e.target.value
-//       }
-//     })
-//   }
-
-//   handleSubmit = async (e) => {
-//     e.preventDefault()
-//     let { id } = this.props.match.params
-//     const created = await createComment(id, this.state.review)
-//     this.setState(prevState => ({
-//       allReviews: [...prevState.allReviews, created]
-//     }))
-//   }
-//   render() {
-
-//     // const filteredRecipe = this.props.commentData.find((recipe) => recipe._id === this.props.match.params.id)
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    })
+  }
 
 
 
-//     return <div className="editReview"><h2>Edit your Review</h2>
-//       {/* <form onSubmit={this.handleSubmit}> */}
-//       <form>
-//         <input type="text" value="hi" onChange={this.handleNameChange} />
-//         <input type="text" value="hi" onChange={this.handleCommentChange} />
-//         <input type="number" value="2" onChange={this.handleRatingChange} />
-//         <button>Submit</button>
-//       </form>
-//     </div>
-//   }
-// }
+  render() {
+    
 
+    const { name, comment, starRating } = this.state
+    const { id, update, hideModal, handleUpdate } = this.props
+    const showModalView = update === id ? 'modal display-block' : 'modal display-none'
 
-// export default withRouter(UpdateReview)
+    return (
+      <div >
+        <div className="w-full max-w-xs z-50">
+          <div className={showModalView}>
+            <section className='modal-main'>
+              <button onClick={hideModal} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline m-1">X</button>
+              <form onSubmit={(e) => {
+                e.preventDefault();
+                handleUpdate(id, this.state);
+                // history.push(`/states/${id}/climbs/${id}/reviews`)
+              }} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 z-20">
+                <h3 className="flex justify-center text-3xl text-blue-500 font-semibold">Update Review</h3>
+                
+                <div className="mb-6">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Rating:
+        <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Name"
+                      type="number"
+                      min="0"
+                      max="5"
+                      name="rating"
+                      value={starRating}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
 
+                <div className="mb-6">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Name:
+        <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Name"
+                      type="text"
+                      name="name"
+                      value={name}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
+                <div className="mb-6">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Comment
+        <textarea className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-20"
+                      placeholder="Comment"
+                      type="text"
+                      name="comment"
+                      value={comment}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                </div>
 
+                
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={hideModal}>Submit</button>
+              </form>
+            </section>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withRouter(UpdateReview)
